395e9bec393ca9cafa6d14aa01c90f07abc8a257
cynthiakem <cynthia.kem449@gmail.com>
2024-04-23 13:38:45 -0400

    Initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,57 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import {logInOutline, personCircleOutline} from 'ionicons/icons';
 
 const Home: React.FC = () => {
+  const doLogin = (event:any) => {
+    event.preventDefault();
+    console.log('doLogin');
+  }
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+        <IonToolbar color={'primary'}>
+          <IonTitle>Demo</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
+        <IonCard>
+          <IonCardContent>
+            <form onSubmit={doLogin}>
+              <IonInput
+              fill='outline'
+              labelPlacement='floating'
+              label='email' type='email'
+              placeholder='example@email.com'>
+              </IonInput>
+              <IonInput
+              className='ion-margin-top'
+              fill='outline'
+              labelPlacement='floating'
+              label='password'
+              type='password'
+              placeholder='password'>
+              </IonInput>
+              <IonButton type='submit' expand='block' className='ion-margin-top'>
+                Login
+                <IonIcon icon={logInOutline} slot='end'/>
+                </IonButton>
+              <IonButton routerLink='/register' color='secondary' type='button' expand='block' className='ion-margin-top'>
+                Create Account 
+                <IonIcon icon={personCircleOutline} slot='end'/>
+              </IonButton>
+            </form>
+          </IonCardContent>
+        </IonCard>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+   
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+
       </IonContent>
     </IonPage>
   );
